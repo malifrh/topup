aa492efd5c26ab0e3966ead005eb7f53b7675a7c
Muhammad Alif Rhamadhan <50443591+malifrh@users.noreply.github.com>
2022-07-03 23:17:51 +0700

    site url 2

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="<?= site_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
